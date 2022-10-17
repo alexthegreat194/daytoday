37a5f7bff8afd8b4b7a54bed2ac12998070e9a80
alexthegreat194 <alexander.gentry@aol.com>
2022-10-17 03:34:18 -0700

    checkin subtasks

--- components/CheckinSubtasks.jsx
+++ components/CheckinSubtasks.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Priority from "./Priority";
 
 const CheckinSubtasks = ({ tasksState, increasePhase }) => {
 
@@ -18,22 +19,47 @@ const CheckinSubtasks = ({ tasksState, increasePhase }) => {
     }
 
     return (
-        <div>
-            <h1>Subtasks</h1>
-            {subTasks.map((task, i) => (
-                <div key={i} className="flex gap-5">
-                    <h3>{task.title}</h3>
-                    <h3>{task.priority}</h3>
-                    <button onClick={() => deleteTask(i)}>Delete</button>
+        <div className="text-xl h-[80vh] flex flex-col items-center justify-center gap-5">
+            <h1 className="text-3xl font-bold">Subtasks</h1>
+
+            <div className="w-3/5 flex flex-col gap-5">
+                <div className="flex flex-col shadow">
+                    {subTasks.map((task, i) => {
+
+                        let roundedCorners = "";
+                        if (i === 0) {
+                            roundedCorners = " rounded-t-xl";
+                        }
+
+                        return (
+                            <div key={i} className={"flex justify-between items-center border p-4 w-full " + roundedCorners}>
+                                <h3>{task.title}</h3>
+
+                                <div className="flex gap-10">
+                                    <Priority priority={task.priority} />
+
+                                    <button onClick={() => deleteTask(i)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                                            className="w-8 h-8 text-red-500">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </div> 
+                        )
+                    })}
+    
+                    <form onSubmit={newTaskHandler} className="flex flex-col">
+                        <input className="w-full p-4 border" type="text" placeholder="Add a task" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} required />
+                        <button className="bg-red-300 text-white rounded-b-xl p-2" type="submit">Add</button>
+                    </form>
                 </div>
-            ))}
 
-            <form onSubmit={newTaskHandler}>
-                <input type="text" placeholder="Add a task" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} required />
-                <button type="submit">Add</button>
-            </form>
-
-            <button onClick={increasePhase}>Done</button>
+                <div className="flex gap-10">
+                    <button className="text-white bg-red-300 hover:bg-red-400 transition-colors rounded-xl px-5 py-2" onClick={increasePhase}>Done</button>
+                    <button className="text-red-300">Clear All</button>
+                </div>
+            </div>
         </div>
     )
 }
